d13eb73b74feffdcb082512a89c05d988757b38f
pattiradu <pattiradu@gmail.com>
2025-01-22 19:02:58 +0200

    employed, the form is added successfully

---
+++ registration.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RP MUSANZE COLLEGE</title>
+</head>
+<body>
+    <h1>RP MUSANZE COLLEGE</h1>
+    <form action="submit_registration.php" method="post">
+        <label for="names">Names:</label><br>
+        <input type="text" id="names" name="names" required><br><br>
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email" required><br><br>
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
