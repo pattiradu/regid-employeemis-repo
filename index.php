f6dcc57194557709f4e23b22d1cb01fcd079e5b7
pattiradu <pattiradu@gmail.com>
2025-01-22 18:56:29 +0200

    index page is created

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Staff MIS</title>
+</head>
+<body>
+    <h1>Welcome to new staff MIS</h1>
+    <a href="registration.php">Go to Registration Page</a>
+</body>
+</html>
